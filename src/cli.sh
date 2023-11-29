014b2a91e3f584fdeb66d492560f0a1b6ca9cfab
Jamon Holmgren <code@jamon.dev>
2023-11-28 22:22:37 -0800

    not dkgray

--- src/cli.sh
+++ src/cli.sh
@@ -14,7 +14,7 @@ main() {
     BLUE='\033[1;34m'
     YELLOW='\033[1;33m'
     RED='\033[1;31m'
-    DKGRAY='\033[1;30m'
+    DKGRAY='\033[0;30m'
     CYAN='\033[1;36m'
     GREEN='\033[1;32m'
     END='\033[0m' # End Color
